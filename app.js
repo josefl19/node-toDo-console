2a2c5c757b03f64e8ee77f54ad42a19b025d0bc0
josefl <jose.fl_@hotmail.com>
2022-09-05 17:45:47 -0500

    Inquirer added. Menu updated with inquirer.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-require('colors');
-const { mostrarMenu, pausa } = require('./helpers/messages');
+import colors from "colors";
+import { inquirerMenu, pausa } from './helpers/inquirer.js';
 
 const main = async () => {
     let opt = '';
 
     do {
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         console.log({ opt })
-        if (opt !== '0') await pausa();     // Si es 0 sale de la app sin mostrar el otro mensaje
+
+        await pausa();
+        //if (opt !== '0') await pausa();     // Si es 0 sale de la app sin mostrar el otro mensaje
     } while (opt !== '0');
 }
 
---
+++ helpers/inquirer.js
@@ -0,0 +1,65 @@
+import inquirer from 'inquirer';
+import colors from "colors";
+
+const preguntas = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué desea hacer?',
+        choices: [
+            {
+                value: '1',
+                name: '1. Crear tarea'
+            },
+            {
+                value: '2',
+                name: '2. Listar tareas'
+            },
+            {
+                value: '3',
+                name: '3. Listar tareas completas'
+            },
+            {
+                value: '4',
+                name: '4. Listar tareas pendientes'
+            },
+            {
+                value: '5',
+                name: '5. Completar tarea(s)'
+            },
+            {
+                value: '6',
+                name: '6. Borrar tarea'
+            },
+            {
+                value: '0',
+                name: '0. Salir'
+            }
+        ]
+    }
+];
+
+const inquirerMenu = async() => {
+    console.log('============================'.blue);
+    console.log('   Selecciona una opción   '.bold.blue);
+    console.log('============================\n'.blue);
+
+    const { opcion } = await inquirer.prompt(preguntas);
+
+    return opcion;
+}
+
+const pausa = async() => {
+    await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'enter',
+            message: `Presione ${ 'ENTER'.bold.green } para continuar`
+        }
+    ]);
+}
+
+export {
+    inquirerMenu,
+    pausa
+}
